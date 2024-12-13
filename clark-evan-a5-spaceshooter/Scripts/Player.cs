18d9d51c659e64b30717f526763083778181db9e
EvanC421 <evan.clark1@mohawkcollege.ca>
2024-12-13 00:46:52 -0500

    add shooting to player controls

--- clark-evan-a5-spaceshooter/Scripts/Player.cs
+++ clark-evan-a5-spaceshooter/Scripts/Player.cs
@@ -1,19 +1,32 @@
 using Godot;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class Player : Node2D
 {
-	private float speed = 400;
+	[Export]
+	private float Speed = 400;
+
+	[Export]
+	private PackedScene LaserShot;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		//Player's gravity variables
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		float moveX = Input.GetAxis("left", "right") * speed * (float)delta;
+		float moveX = Input.GetAxis("left", "right") * Speed * (float)delta;
 		Translate(new Vector2(moveX, 0));
+
+		if (Input.IsActionJustPressed("shoot")) //player shoots laser
+		{
+			Node2D laser = LaserShot.Instantiate<Node2D>();
+			this.AddChild(laser);
+		}
 	}
 }
